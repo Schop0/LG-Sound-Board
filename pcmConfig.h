6c3eac75446f32520ec0a0065edbadbd396402e9
Peter Buijs <git@peterbuijs.eu>
2022-11-19 23:02:12 +0100

    TMRpcm library config header

---
+++ pcmConfig.h
@@ -0,0 +1,11 @@
+// See https://github.com/TMRh20/TMRpcm/wiki for info on usage
+
+#ifndef pcmConfig_h
+#define pcmConfig_h
+
+#include <Arduino.h>
+
+#define SD_FULLSPEED
+#define SDFAT
+
+#endif // pcmConfig_h
