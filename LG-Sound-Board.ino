7eb7bfc744f3b1933ed4c49b8e268f8f0bb0db66
Peter Buijs <git@peterbuijs.eu>
2022-12-18 01:09:18 +0100

    Move leds+buttons grid code into separate source files

--- LG-Sound-Board.ino
+++ LG-Sound-Board.ino
@@ -1,52 +1,9 @@
-#define GRID_SIZE 4
 #define SD_ChipSelectPin 2
 #define SPEAKER_PIN 9
 #define AMP_SHUTDOWN_PIN 8
 
 #include "LG-Audio.h"
-
-#include <SimpleKeypad.h>
-
-#define KEY_NONE 0
-#define LED_NONE 0
-
-static const uint8_t ROW_PIN[GRID_SIZE] = {A0, A1, A2, A3};
-static const uint8_t COL_PIN[GRID_SIZE] = { 4, 5, 6, 7 };
-static const char key_chars[GRID_SIZE][GRID_SIZE] = {
-  { 1, 2, 3, 4 },
-  { 5, 6, 7, 8 },
-  { 9, 10, 11, 12 },
-  { 13, 14, 15, 16 }
-};
-
-static bool pixels[GRID_SIZE][GRID_SIZE];
-SimpleKeypad keypad((char *)key_chars, ROW_PIN, COL_PIN, GRID_SIZE, GRID_SIZE);
-
-void setup_grid(uint8_t rowMode) {
-  for (size_t i = 0; i < GRID_SIZE; i++) {
-    pinMode(ROW_PIN[i], rowMode);
-    digitalWrite(COL_PIN[i], HIGH);
-    pinMode(COL_PIN[i], INPUT);
-  }
-}
-
-/*
- * Scan a key matrix
- * Return the number of a single pressed key or KEY_NONE
- */
-uint8_t get_key() {
-  setup_grid(INPUT_PULLUP);
-  return keypad.getKey();
-}
-
-void set_led(uint8_t led) {
-  led--;
-  uint8_t x = led % GRID_SIZE;
-  uint8_t y = led / GRID_SIZE;
-
-  // Toggle pixel
-  pixels[x][y] = !pixels[x][y];
-}
+#include "LG-ledBtnGrid.h"
 
 uint8_t IRDebugPin = 0;
 ISR(ANALOG_COMP_vect)
@@ -85,31 +42,6 @@ void setup() {
   // Point any remote control at the sensor and watch it's signal on the Arduino led
     test_infrared_receiver(LED_BUILTIN);
   }
-}
-
-void leds_refresh() {
-  static size_t row = 0;
-
-  // Disable all leds
-  setup_grid(INPUT);
-
-  // Write pixels to columns
-  for (size_t col = 0; col < GRID_SIZE; col++) {
-    if (pixels[col][row])
-    {
-      digitalWrite(COL_PIN[col], HIGH);
-      pinMode(COL_PIN[col], OUTPUT);
-    }
-  }
-
-  // Activate row for a short time, blocking
-  pinMode(ROW_PIN[row], OUTPUT);
-  digitalWrite(ROW_PIN[row], LOW);
-  delay(1);
-  pinMode(ROW_PIN[row], INPUT);
-
-  // Prepare the next row number
-  ++row %= GRID_SIZE;
 }
 
 void loop() {
