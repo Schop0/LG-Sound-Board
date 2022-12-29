dfd097980c47a8987f1479884d44d2d73d38f870
Peter Buijs <git@peterbuijs.eu>
2022-12-30 00:24:49 +0100

    Rewrite fire

--- LG-Sound-Board.ino
+++ LG-Sound-Board.ino
@@ -33,57 +33,53 @@ void setup() {
   Serial.begin(115200);
 }
 
+unsigned int irPlayerCode = 0;
+int irBitCount = 0;
+bool irBusy = false;
+
 void runLasergame()
 {
-  digitalWrite(PIN_A4, !digitalRead(PIN_A4));
-  if(digitalRead(PIN_A4))
+  int bitTime_ms = 10;
+  if ((time + bitTime_ms) <= millis())
   {
-    irLedOn();
-    digitalWrite(LED_BUILTIN, HIGH);
-  } else {
-    irLedOff();
-    digitalWrite(LED_BUILTIN, LOW);
-  }
-}
+    time += bitTime_ms;
 
-void fire() {
-  unsigned char personID = 0b10100010;
-  int bitnr = 0;
-
-  while (bitnr < 8)
-  {
-    if (personID & 0x01)
-    {
-      // 1
-      sendBit(1);
-    }
-    else
-    {
-      // 0
-      sendBit(0);
-    }
-
-    bitnr++;
-    personID >>= 1;
-  }
-}
-
-void sendBit(uint8_t bit){
-  if ((time + 1000) <= millis())
-  {
-    if (bit==0) {
-      digitalWrite(PIN_A4, LOW);
+    if (irBitCount+1) {
+      if(irPlayerCode & (1 << irBitCount)) {
+        irLedOn();
+        digitalWrite(PIN_A4, HIGH); // Debug
+      } else {
+        irLedOff();
+        digitalWrite(PIN_A4, LOW); // Debug
+      }
+      irBitCount--;
     } else {
-      digitalWrite(PIN_A4, HIGH);
+        irLedOff();
+        digitalWrite(PIN_A4, LOW); // Debug
     }
-    
-    time = millis();
+  }
+}
+
+bool fireLaser(unsigned int playerNumber) {
+  // LG-protocol: 8-bit codes starting with 0b1010 and a 4-bit plyer number
+  // Skip player numbers that may be misinterpreted ad the start bits
+  static const char player[] = {0x00, 0xA0, 0xA1, 0xA2, 0xA3, 0xA4, /*skip*/ 0xA6, 0xA7, 0xA8, 0xA9, /*skip*/ 0xAB, 0xAC, /*skip*/ 0xAE, 0xAF};
+  static const unsigned int playerCount = sizeof(player) / sizeof(player[0]);
+  static const unsigned int playerCodeLength_bits = 8;
+
+  if (playerNumber >= playerCount) {
+    return false;
+  } else
+  if (irBusy) {
+    return false;
+  } else {
+    irPlayerCode = player[playerNumber];
+    irBitCount = playerCodeLength_bits;
+    return true;
   }
 }
 
 void loop() {
-
-
   static uint8_t active_led = LED_NONE;
   static uint8_t previous_key = LED_NONE;
   static uint8_t sound_bank_counter = 0;
@@ -98,8 +94,6 @@ void loop() {
         active_led = LED_NONE;
       }
     }
-  } else if (active_key == 2) {
-    fire();
   } else if (active_key == 1) {
     // switch mode
     // press first button for changing sound bank
@@ -123,7 +117,13 @@ void loop() {
     audio.play(soundFileFromKey(sound_bank_counter, active_key));
 
     previous_key = active_key;
+
+    // Use key number as lasergame player number
+    fireLaser(active_key);
   }
+
+  // Run the lasergame background process to handle transmitting
+  runLasergame();
 
   // Amplifier shutdown control
   digitalWrite(AMP_SHUTDOWN_PIN, !audio.isPlaying());
